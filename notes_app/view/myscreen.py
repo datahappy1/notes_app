e98945dba0b47e466f6d0d796a4543cbeb72a02d
Pavel.Prudky <Hogofogo83>
2022-04-06 13:05:04 +0200

    continuing work on navigation drawer with sections

--- notes_app/view/myscreen.py
+++ notes_app/view/myscreen.py
@@ -1,6 +1,6 @@
+import re
 from enum import Enum
 from os import path, linesep
-import re
 
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -10,8 +10,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
+from kivymd.theming import ThemableBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import TwoLineListItem, OneLineIconListItem
+from kivymd.uix.list import TwoLineListItem, OneLineIconListItem, MDList
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.snackbar import BaseSnackbar
@@ -33,20 +34,21 @@ class ItemDrawer(OneLineIconListItem):
 
 # TODO
 # https://kivymd.readthedocs.io/en/latest/components/navigationdrawer/index.html
-class ContentNavigationDrawer(MDBoxLayout):
-    def on_start(self):
-        icons_item = {
-            "folder": "My files",
-            "account-multiple": "Shared with me",
-            "star": "Starred",
-            "history": "Recent",
-            "checkbox-marked": "Shared with me",
-            "upload": "Upload",
-        }
-        for icon_name in icons_item.keys():
-            self.root.ids.content_drawer.ids.md_list.add_widget(
-                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
-            )
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """
+        Called when tap on a menu item
+        Set the color of the icon and text for the menu item.
+        """
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
 
 
 class OpenFilePopup(FloatLayout):
@@ -114,6 +116,25 @@ class MyScreenView(BoxLayout, MDScreen, Observer):
         self.load_initial_data_split_by_section()
         self.section_names = []
         self.get_section_names()
+        self.set_drawer_items()
+
+    def set_drawer_items(self):
+        # icons_item = {
+        #     "folder": "My files",
+        #     "account-multiple": "Shared with me",
+        #     "star": "Starred",
+        #     "history": "Recent",
+        #     "checkbox-marked": "Shared with me",
+        #     "upload": "Upload",
+        # }
+        # for icon_name in icons_item.keys():
+        #     self.ids.md_list.add_widget(
+        #         ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+        #     )
+        for section_name in self.section_names:
+            self.ids.md_list.add_widget(
+                ItemDrawer(icon="star", text=section_name)
+            )
 
     def load_initial_data(self):
         self.text_view.text = self.controller.read_file_data()
